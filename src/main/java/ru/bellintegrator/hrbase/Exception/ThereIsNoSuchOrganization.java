c485b33087816b072b2c0818444232067c21ebd8
maratimaev <mailto:imaevmarat@outlook.com>
2018-12-16 21:19:35 +0500

    rename exception

--- src/main/java/ru/bellintegrator/hrbase/Exception/ThereIsNoSuchOrganization.java
+++
@@ -1,7 +0,0 @@
-package ru.bellintegrator.hrbase.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-public class ThereIsNoSuchOrganization extends RuntimeException {
-}
